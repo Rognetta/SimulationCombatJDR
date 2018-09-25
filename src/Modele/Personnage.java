7011d6cbbe2426809c2022a317a97fbcb5858a06
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2018-09-25 08:39:41 +0200

    ajout Class personnage

---
+++ src/Modele/Personnage.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Personnage {
+    protected int chance;
+    protected int chance_max;
+    protected int force;
+    protected int force_bonus;
+    protected int endurance;
+    protected int endurance_bonus;
+    protected int agilité;
+    protected int agilité_bonus;
+    protected int intelligence;
+    protected int intelligence_bonus;
+    protected int trempe;
+    protected int trempe_bonus;
+    protected int social;
+    protected int social_bouns;
+    protected int pv;
+    protected int pv_max;
+    protected int coeffRegenPv;
+    protected int mana;
+    protected int manaMax;
+    //-- magies
+    protected int coeffRegenMana;
+    protected int armeDistance;
+    protected int armeDistance_bonus;
+    protected int discretion;
+    protected int discretion_bonus;
+    protected int dressage;
+    protected int dressage_bonus;
+    protected int mélée;
+    protected int mélée_bonus;
+    protected int nage;
+    protected int nage_bonus;
+    protected int premiersSoin;
+    protected int premiersSoin_bonus;
+    protected int coercition; // calcul auto
+    protected int coercition_bonus;
+    protected int mouvement; // calcul auto
+    protected int mouvement_bonus;
+    protected int perception; // calcul auto
+    protected int perception_bonus;
+    protected int persuation; // calcul auto
+    protected int persuation_bonus;
+    protected int volonté; // calcul auto
+    protected int volonté_bonus;
+    protected int dégats;
+    protected int dégats_bonus; // calcul auto
+    protected String typeDégat;
+    protected int armurePercant;
+    protected int armureContondant;
+    protected int armureTranchant;
+    //--! A prevoir
+    // armure mutation
+    // système equivalent toggle des differentes pièces d'armures
+
+    public Personnage() {
+    }
+    
+    
+}

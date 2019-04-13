1425407d89693fdc0046ea663aeb7e0b123edec1
FLAMANT Thomas <32953767+syvtoto@users.noreply.github.com>
2019-04-13 17:43:04 +0200

    push du main

--- src/Controleur/Main.java
+++ src/Controleur/Main.java
@@ -55,27 +55,27 @@ public class Main {
 //
 //        System.out.println("Pourcentage de c1 = " + count1/10f + " plus ou moins 2 pourcent");
 //        System.out.println("Pourcentage de c2 = " + count2/10f + " plus ou moins 2 pourcent");
-        Database database = new Database();
-        try {
-            database.writeDbToFile();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Database size after set : " + database.getM_database().size());
-        try {
-            database.readDbFromFile();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Database size : " + database.getM_database().size());
-        database.addToDatabase(Gen.PaysanGen());
-        System.out.println("Database size after insert : " + database.getM_database().size());
-        try {
-            database.writeDbToFile();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Database size after get : "+database.getM_database().size());
-//        MainPanel paneltest = new MainPanel("Gestion de combat");
+//        Database database = new Database();
+//        try {
+//            database.writeDbToFile();
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        System.out.println("Database size after set : " + database.getM_database().size());
+//        try {
+//            database.readDbFromFile();
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        System.out.println("Database size : " + database.getM_database().size());
+//        database.addToDatabase(Gen.PaysanGen());
+//        System.out.println("Database size after insert : " + database.getM_database().size());
+//        try {
+//            database.writeDbToFile();
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        System.out.println("Database size after get : "+database.getM_database().size());
+        MainPanel paneltest = new MainPanel("Gestion de combat");
     }
 }
